a1d8e218ec0dc1799b595536466a75b57fd89a48
Konstantinos_Lykoudis <konstantinoslyk@gmail.com>
2024-03-20 20:05:42 +0200

    create frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(800, 800);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }

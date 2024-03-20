d55669dd2fd531d628d66221d38e26252087e279
Konstantinos_Lykoudis <konstantinoslyk@gmail.com>
2024-03-20 21:34:46 +0200

    create SnakeGame class

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = 600;
+
         JFrame frame = new JFrame("Snake");
         frame.setVisible(true);
-        frame.setSize(800, 800);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
     }
 }
---
+++ src/SnakeGame.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class SnakeGame extends JPanel{
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }       
+    }
+
+    int boardWidth;
+    int boardHeight;
+    int tileSize = 25;
+
+    SnakeGame(int boardWidth, int boardHeight) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.gray);
+
+    }
+}
